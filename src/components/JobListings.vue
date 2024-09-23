ae807b9ed410b111139b759d0e8b3089ffbb3c3d
moses197 <leomoszeko@gmail.com>
2024-09-23 09:21:18 +0100

    asrfgs

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink } from 'vue-router';
-import JobData from '@/jobs.json';
+// import JobData from '@/jobs.json';
 import JobListing from './JobListing.vue';
-import { ref, defineProps } from 'vue';
+import { reactive, ref, defineProps, onMounted } from 'vue';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
+import axios from 'axios';
 
-const jobs = ref(JobData);
+// const jobs = ref(JobData);
 // console.log(jobs.value)
 
 defineProps({
@@ -14,14 +16,41 @@ defineProps({
         default: false,
     },
 })
+
+// const jobs = ref([]);
+const state = reactive({
+    jobs: [],
+    isLoading: true
+})
+
+onMounted(async () => {
+    try {
+        const response = await axios.get('/api/jobs');
+        // jobs.value = response.data;
+        state.jobs = response.data;
+        console.log(jobs.value)
+    } catch {
+        console.error("Error fetching jobs", error)
+    } finally {
+        state.isLoading = false
+    }
+});
 </script>
 
 <template>
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto">
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse jobs</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/>
+            <!-- Show loading spinner while loading is true -->
+             <div v-if="state.isLoading" class="text-center text-gray-500 py-6 ">
+                <PulseLoader />
+             </div>
+
+
+             <!-- Show job listing when done loading -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <!-- <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job"/> -->
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job"/>
             </div>
         </div>
     </section>
